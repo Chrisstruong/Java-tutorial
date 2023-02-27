ea3f77b3b0d29aabb9905f5c27fc1d5226ab54ac
chrisstruong <chrisstruong@gmail.com>
2023-02-27 12:53:17 -0500

    Finish user input

---
+++ GetUserInput.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class GetUserInput {
+    public static void main(String[] args){
+        // Scanner input object
+        Scanner input = new Scanner (System.in);
+
+        // Output the prompt
+        System.out.println("Enter a floating point value:");
+
+        // Wait for the user to enter something
+        double value = input.nextDouble();
+
+        // Tell them what they entered.
+        System.out.println("You entered: "+value);
+    }
+}
