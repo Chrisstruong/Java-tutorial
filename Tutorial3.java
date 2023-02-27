765b974bfc5ec33603884a730f2e3d1850de0763
chrisstruong <chrisstruong@gmail.com>
2023-02-27 12:12:39 -0500

    work on walkthrough turotials

---
+++ Tutorial3.java
@@ -0,0 +1,15 @@
+public class Tutorial3 {
+    public static void main(String[] args) {
+        int myInt = 7;
+        String text = "Hello";
+        String blank = " ";
+        String name = "Bob";
+        String greeting = text + blank + name;
+
+        System.out.println(greeting);
+        System.out.println("Hello"+" "+"Bob");
+        System.out.println("My integer is: " + myInt);
+        double myDouble = 7.123;
+        System.out.println("My number is: " +myDouble+".");
+    }
+}
