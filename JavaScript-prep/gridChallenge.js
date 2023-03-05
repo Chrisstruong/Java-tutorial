0e515c83dc1c00535c476257bd12164d9fa556f0
chrisstruong <chrisstruong@gmail.com>
2023-03-05 11:48:39 -0500

    Finish grid challenge

--- JavaScript-prep/gridChallenge.js
+++ JavaScript-prep/gridChallenge.js
@@ -1,15 +1,32 @@
 function gridChallenge(grid) {
     // Write your code here
     // const alphaLetters = "abcdefghijklmnopqrstuvwxyz"
+
+    // for (let i=0; i < grid.length; i++) {
+    //     grid[i] = grid[i].split('').sort().join('')
+    // }
+    // console.log(grid)
+    // for (let j=1; j < grid.length; j++) {
+    //     for (let k=0; k < grid[j].length; k++) {
+    //         console.log(grid[j].charCodeAt(k))
+    //         console.log(grid[j-1][grid[j].length-1])
+    //         if (grid[j].charCodeAt(k) < grid[j-1].charCodeAt(grid[j].length-1)) return "NO"
+    //     }
+    // }
+    // return "YES"
+
     for (let i=0; i < grid.length; i++) {
         grid[i] = grid[i].split('').sort().join('')
     }
-    for (let j=1; j < grid.length; j++) {
-        for (let k=0; k < grid[j].length; k++) {
-            if (grid[j].charCodeAt(k) < grid[j-1][grid[j].length-1]) return "NO"
+    console.log(grid)
+    
+    for (let k = 0; k < grid.length; k++){
+        for (let j = 1; j < grid.length; j ++){
+            let tempNum = grid[j-1].charCodeAt(k)
+            if (tempNum > grid[j].charCodeAt(k)) return "NO"
         }
     }
     return "YES"
 }
 
-console.log(gridChallenge(['mpxz', 'abcd', 'wlmf']))
+console.log(gridChallenge(['kc', 'iu']))
