a0789c02771ea7c9aeca549f91ebec92999a05b3
chrisstruong <chrisstruong@gmail.com>
2023-03-04 19:44:48 -0500

    Almost get palindromeindex

--- JavaScript-prep/palindromeIndex.js
+++ JavaScript-prep/palindromeIndex.js
@@ -6,8 +6,8 @@ function palindromeIndex(s) {
     if (slen >= 1 && slen <= 100005 & s!==s.split('').reverse().join('')){
         for (let i=0; i < slen; i++){
             if (s.charAt(i) != s.charAt(slen -1 -i)){
-                let s1 = s.substring(0,i) + substring((i+1))
-                let s2 = s.substring(0,(slen -1 -i)) +s.substring((len -1 -i) +1)
+                let s1 = s.substring(0,i) + s.substring((i+1))
+                let s2 = s.substring(0,(slen -1 -i)) +s.substring((slen -1 -i) +1)
                 if (s1 === s1.split('').reverse().join('')){
                     result = i
                 } else if (s2 === s2.split('').reverse().join('')) {
@@ -21,4 +21,4 @@ function palindromeIndex(s) {
 
 }
 
-console.log(palindromeIndex("abcd"))
+console.log(palindromeIndex("abcdeqwerdf"))
