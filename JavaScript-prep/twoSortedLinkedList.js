fb30248922e22c4f0dceac015cc515318198c622
chrisstruong <chrisstruong@gmail.com>
2023-03-07 14:55:23 -0500

    Finish two sorted linked list by myself

--- JavaScript-prep/twoSortedLinkedList.js
+++ JavaScript-prep/twoSortedLinkedList.js
@@ -1,5 +1,3 @@
-
-
 function mergeLists(head1, head2) {
     let curr = new SinglyLinkedListNode()
     const dummy = curr
@@ -19,9 +17,100 @@ function mergeLists(head1, head2) {
     if (head2) {
         curr.next = head2
     }
-    console.log(dummy)
+    console.log(JSON.stringify(dummy))
     return dummy.next
 
 }
 
-console.log(mergeLists())
+class Node {
+    constructor(key) {
+        this.key = key
+        this.next = null
+    }
+}
+
+function newNode(key){
+    let temp = new Node(key)
+    return temp
+}
+
+let a = newNode(5)
+a.next = newNode(10)
+a.next.next = newNode(15)
+a.next.next.next = newNode(40)
+
+let b = newNode(2)
+b.next = newNode(3)
+b.next.next = newNode(20)
+let v = []
+while (a !== null) {
+    v.push(a.key)
+    a = a.next
+}
+
+while (b !== null) {
+    v.push(b.key)
+    b = b.next
+}
+v.sort((a,b)=>a-b)
+// console.log(v)
+
+let result = newNode(-1)
+let temp = result //when result get updated, temp also get updated
+
+// store linkedlist to "result"
+for (let i=0; i < v.length; i++){
+    result.next = newNode(v[i])
+    result = result.next
+}
+
+temp = temp.next
+console.log(temp)
+console.log(`Resultant Merge Linked List is: `)
+while(temp!= null) {
+    // console.log(temp.key + " ")
+    temp = temp.next
+}
+
+
+
+
+// function mergeLists(head1, head2) {
+//     class Node {
+//         constructor(key){
+//             this.key = key
+//             this.next = null
+//         }
+//     }
+    
+//     function newNode(key) {
+//         let temp = new Node(key)
+//         return temp
+//     }
+    
+//     const v = []
+//     while(head1 != null) {
+//         v.push(head1.data)
+//         head1 = head1.next
+//     }
+//     while(head2 != null) {
+//         v.push(head2.data)
+//         head2 = head2.next
+//     }
+//     v.sort((a,b)=>a-b)
+//     console.log(v)
+    
+//     let result = newNode(-1)
+//     let temp = result
+    
+//     for (let i=0; i < v.length; i++) {
+//         result.next = newNode(v[i])
+//         result = result.next
+//     }
+//     temp = temp.next
+//     while(temp !=null) {
+//         console.log(temp.key)
+//         temp = temp.next
+//     }
+
+// }
