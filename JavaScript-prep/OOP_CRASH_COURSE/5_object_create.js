60808a15c53998b200e699bc28247952f16e46f7
chrisstruong <chrisstruong@gmail.com>
2023-03-08 09:53:01 -0500

    Finish object create

--- JavaScript-prep/OOP_CRASH_COURSE/5_object_create.js
+++ JavaScript-prep/OOP_CRASH_COURSE/5_object_create.js
@@ -1,22 +1,48 @@
-// Object of Protos
-const bookPhotos = {
-    getSummary: function(){
+// // Object of Protos
+// const bookPhotos = {
+//     getSummary: function(){
+//         return `${this.title} was written by ${this.author} in ${this.year}`
+//     },
+//     getAge: function(){
+//         const years = new Date().getFullYear() - this.year
+//         return `${this.title} is ${years} years old`
+//     }
+// }
+
+// // Create Object 
+// // const book1 = Object.create(bookPhotos)
+// // book1.title = 'Book One'
+// // book1.author = 'John Doe'
+// // book1.year = '2013'
+
+// const book1 = Object.create(bookPhotos, {
+//     title: { value: 'Book One '},
+//     author: { value: 'John Doe'},
+//     year: { value: '2013'}
+// })
+
+// console.log(book1)
+
+
+// Object Of Protos
+const bookProtos = {
+    getSummary: function() {
         return `${this.title} was written by ${this.author} in ${this.year}`
     },
-    getAge: function(){
+    getAge: function() {
         const years = new Date().getFullYear() - this.year
         return `${this.title} is ${years} years old`
     }
 }
 
-// Create Object 
-// const book1 = Object.create(bookPhotos)
-// book1.title = 'Book One'
-// book1.author = 'John Doe'
-// book1.year = '2013'
+// Create Object
+const book1 = Object.create(bookProtos)
+book1.title= 'Book One'
+book1.author = 'John Doe'
+book1.year = '2013'
 
-const book1 = Object.create(bookPhotos, {
-    title: { value: 'Book One '},
+const book1 = Object.create(bookProtos, {
+    title: {value: 'Book One'},
     author: { value: 'John Doe'},
     year: { value: '2013'}
 })
