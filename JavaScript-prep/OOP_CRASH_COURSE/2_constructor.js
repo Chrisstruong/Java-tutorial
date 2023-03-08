a818ce2abffbac3bf009cace05c6c1c268e21602
chrisstruong <chrisstruong@gmail.com>
2023-03-08 09:28:25 -0500

    Finish constructor

--- JavaScript-prep/OOP_CRASH_COURSE/2_constructor.js
+++ JavaScript-prep/OOP_CRASH_COURSE/2_constructor.js
@@ -1,16 +1,33 @@
-// Constructor
+// // Constructor
 
+// function Book(title, author, year){
+//     this.title = title
+//     this.author = author
+//     this.year = year
+//     this.getSummary = function(){
+//         return `${this.title} was written by ${this.author} in ${this.year}`
+//     }
+// }
+
+// // Instantiate an object
+// const book1 = new Book('Book One', 'John Doe', '2013')
+// const book2 = new Book('Book Two', 'Jane Doe', '2016')
+
+// console.log(book2.getSummary())
+
+// Constructor
 function Book(title, author, year){
     this.title = title
     this.author = author
     this.year = year
+
     this.getSummary = function(){
         return `${this.title} was written by ${this.author} in ${this.year}`
     }
 }
 
-// Instantiate an object
-const book1 = new Book('Book One', 'John Doe', '2013')
+// Instantiate an Object
+const book1 = new Book('Book One', 'John Doe', '2013') // Instantiate an Object is running through the whole constructor
 const book2 = new Book('Book Two', 'Jane Doe', '2016')
 
-console.log(book2.getSummary())
+console.log(book2)
