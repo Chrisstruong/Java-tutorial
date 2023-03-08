4d8e421266d9ee81f2fa4b6a68ab27278a986791
chrisstruong <chrisstruong@gmail.com>
2023-03-08 08:55:07 -0500

    Rework on basic literals

--- JavaScript-prep/OOP_CRASH_COURSE/1_basic_literals.js
+++ JavaScript-prep/OOP_CRASH_COURSE/1_basic_literals.js
@@ -8,11 +8,45 @@
 // alert(1)
 // console.log(navigator.appVersion)
 
-// Object Literal
+// // Object Literal
+// const book1 = {
+//     title: 'Book One',
+//     author: "John Doe",
+//     year: "2013",
+//     getSummary: function(){
+//         return `${this.title} was written by ${this.author} in ${this.year}`
+//     }
+// }
+
+// const book2 = {
+//     title: 'Book Two',
+//     author: "John Doe",
+//     year: "2016",
+//     getSummary: function(){
+//         return `${this.title} was written by ${this.author} in ${this.year}`
+//     }
+// }
+
+
+// // console.log(book2.getSummary())
+// console.log(Object.values(book2))
+// console.log(Object.keys(book2))
+
+
+// const s1 = 'hello'
+// console.log(typeof s1)
+
+// const s2 = new String('Hello')
+// console.log(typeof s2)
+
+// console.log(window)
+// window.alert(1)
+// console.log(navigator.appVersion)
+
 const book1 = {
     title: 'Book One',
-    author: "John Doe",
-    year: "2013",
+    author: 'John Doe',
+    year: '2013',
     getSummary: function(){
         return `${this.title} was written by ${this.author} in ${this.year}`
     }
@@ -20,14 +54,12 @@ const book1 = {
 
 const book2 = {
     title: 'Book Two',
-    author: "John Doe",
-    year: "2016",
+    author: 'Jane Doe',
+    year: '2016',
     getSummary: function(){
         return `${this.title} was written by ${this.author} in ${this.year}`
     }
 }
 
-
-// console.log(book2.getSummary())
-console.log(Object.values(book2))
-// console.log(Object.keys(book2))
+console.log(book2.getSummary())
+console.log(Object.keys(book2))
