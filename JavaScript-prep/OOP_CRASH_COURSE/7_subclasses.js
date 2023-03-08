f579e03eaa00438798fa48d345fa577404a542aa
chrisstruong <chrisstruong@gmail.com>
2023-03-08 10:03:56 -0500

    finish subclasses

--- JavaScript-prep/OOP_CRASH_COURSE/7_subclasses.js
+++ JavaScript-prep/OOP_CRASH_COURSE/7_subclasses.js
@@ -1,23 +1,48 @@
+// class Book {
+//     constructor(title, author, year) {
+//         this.title = title
+//         this.author = author
+//         this.year = year
+//     }
+//     getSummary() {
+//         return `${this.title} was written by ${this.author} in ${this.year}`
+//     }
+// }
+
+// // Magazine Subclass
+// class Magazine extends Book{
+//     constructor(title, author, year, month) {
+//         super(title, author, year)
+//         this.month = month
+//     }
+// }
+
+// // Instantiate Magazine
+// const mag1 = new Magazine('Magazine', 'John Doe', '2018', 'Jan')
+
+// console.log(mag1.getSummary())
+
+
 class Book {
     constructor(title, author, year) {
         this.title = title
         this.author = author
         this.year = year
     }
+
     getSummary() {
         return `${this.title} was written by ${this.author} in ${this.year}`
     }
 }
 
 // Magazine Subclass
-class Magazine extends Book{
+class Magazine extends Book {
     constructor(title, author, year, month) {
-        super(title, author, year)
+        super(title, author, year);//Copy Book constructor properties
         this.month = month
     }
 }
 
 // Instantiate Magazine
-const mag1 = new Magazine('Magazine', 'John Doe', '2018', 'Jan')
-
+const mag1 = new Magazine('Mag One', 'John Doe', '2018', 'Jan')
 console.log(mag1.getSummary())
