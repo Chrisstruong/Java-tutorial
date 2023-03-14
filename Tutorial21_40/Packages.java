82ee291e24624b40371562acdca9d752a25fe638
chrisstruong <chrisstruong@gmail.com>
2023-03-14 12:17:44 -0400

    Work on package java

---
+++ Tutorial21_40/Packages.java
@@ -0,0 +1,8 @@
+package Tutorial21_40;
+import ocean.Fish; //.* import all classes from that package
+import ocean.plants.Seaweed;
+
+public class Packages {
+    Fish fish = new Fish();
+    Seaweed weed = new Seaweed();
+}
---
+++ Tutorial21_40/Seaweed.java
@@ -0,0 +1,5 @@
+package Tutorial21_40;
+
+public class Seaweed {
+
+}
